606cde0836afa0f0bafd20924f47dbabe568ad2d
lnguyen23ssis <94580413+lnguyen23ssis@users.noreply.github.com>
2022-02-10 09:11:51 +0700

    Update cp_task_3.py

--- cp_light_control/cp_task_3.py
+++ cp_light_control/cp_task_3.py
@@ -2,5 +2,7 @@ import time
 import board
 from analogio import AnalogIn, AnalogOut
 
-light = AnalogIn(board.A1)
-light.value = AnalogOut(board.A0)
+while True:
+  light = AnalogIn(board.A1)
+  light.value = AnalogOut(board.A0)
+
