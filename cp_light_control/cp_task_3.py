44b0c3c3a1713ef104cc8fa04d06dc6e12f858ad
lnguyen23ssis <94580413+lnguyen23ssis@users.noreply.github.com>
2022-02-11 09:43:40 +0700

    Update cp_task_3.py

--- cp_light_control/cp_task_3.py
+++ cp_light_control/cp_task_3.py
@@ -5,12 +5,12 @@ from analogio import AnalogIn
 from analogio import AnalogOut
 
 light = AnalogIn(board.A1)
-red = AnalogOut(board.A0)
+brightness = AnalogOut(board.A0)
 
   
 
 while True:
     print(light.value)
     time.sleep(0.5)
-    red.value = light.value
+    brightness.value = light.value
    
